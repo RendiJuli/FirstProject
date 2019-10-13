833b581be66cf7179545e223d434c94368b2e59c
RendiJuli <juliarto.rendi@gmail.com>
2019-10-13 20:19:09 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/example/moviecatalagoue/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.moviecatalagoue;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.moviecatalagoue.Fragment.MoviesFragment;
+import com.example.moviecatalagoue.Fragment.TVShowFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_movies:
+                    fragment = new MoviesFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+                case R.id.navigation_tvshow:
+                    fragment = new TVShowFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState == null){
+            navView.setSelectedItemId(R.id.navigation_movies);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/moviecatalagoue/adapter/MoviesAdapter.java
@@ -0,0 +1,94 @@
+package com.example.moviecatalagoue.adapter;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import com.example.moviecatalagoue.R;
+import com.example.moviecatalagoue.activity.MoviesDetailActivity;
+import com.example.moviecatalagoue.model.Movies;
+
+import java.util.ArrayList;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    private ArrayList<Movies> mData = new ArrayList<>();
+    public void setData(ArrayList<Movies> items) {
+        mData.clear();
+        mData.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int position) {
+        View mView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_cardview_movies, viewGroup, false);
+        return new MovieViewHolder(mView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder movieViewHolder, int position) {
+        movieViewHolder.bind(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        ImageView imgPhoto;
+        TextView textViewTitle, textViewDateReleased,
+                textViewVoteAverage, textViewVoteCount;
+
+        MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.tv_item_title);
+            textViewDateReleased = itemView.findViewById(R.id.tv_item_dateReleased);
+            textViewVoteAverage = itemView.findViewById(R.id.tv_item_voteAverege);
+            textViewVoteCount = itemView.findViewById(R.id.tv_item_voteCount);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+
+
+            itemView.setOnClickListener(this);
+        }
+        void bind(Movies movies) {
+            String vote_average = Double.toString(movies.getVote_average());
+            String url_image = "https://image.tmdb.org/t/p/w185" + movies.getPhoto();
+
+
+            textViewTitle.setText(movies.getTitle());
+            textViewDateReleased.setText(movies.getRelease_date());
+            textViewVoteAverage.setText(vote_average);
+            textViewVoteCount.setText(movies.getVote_count());
+
+
+            Glide.with(itemView.getContext())
+                    .load(url_image)
+                    .placeholder(R.color.colorAccent)
+                    .dontAnimate()
+                    .into(imgPhoto);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            Movies movie = mData.get(position);
+//
+            movie.setTitle(movie.getTitle());
+            movie.setOverview(movie.getOverview());
+
+            Intent moveWithObjectIntent = new Intent(itemView.getContext(), MoviesDetailActivity.class);
+            moveWithObjectIntent.putExtra(MoviesDetailActivity.EXTRA_MOVIE, movie);
+            itemView.getContext().startActivity(moveWithObjectIntent);
+        }
+    }
+
+
+}
